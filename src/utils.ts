0fea7e97261e59280cb2f6b1bf819514d2280837
MihailPetkov96 <mpetkov228@gmail.com>
2024-12-23 18:01:51 +0200

    Refactor request body parsers

--- src/utils.ts
+++ src/utils.ts
@@ -1,51 +1,6 @@
+import { z } from 'zod';
+
 import { Gender, NewPatient } from "./types";
-
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender).map(g => g.toString()).includes(param);
-};
-
-const parseName = (param: unknown): string => {
-  if (!isString(param)) {
-    throw new Error('Incorrect or missing name');
-  }
-  return param;
-};
-
-const parseDateOfBirth = (param: unknown): string => {
-  if (!isString(param) || !isDate(param)) {
-    throw new Error('Incorrect or missing date of birth');
-  }
-  return param;
-};
-
-const parseSsn = (param: unknown): string => {
-  if (!isString(param)) {
-    throw new Error('Incorrect or missing ssn');
-  }
-  return param;
-};
-
-const parseGender = (param: unknown): Gender => {
-  if (!isString(param) || !isGender(param)) {
-    throw new Error('Incorrect or missing gender');
-  }
-  return param;
-};
-
-const parseOccupation = (param: unknown): string => {
-  if (!isString(param)) {
-    throw new Error('Incorrect or missing occupation');
-  }
-  return param;
-};
 
 const toNewPatient = (object: unknown): NewPatient => {
   if (!object || typeof object !== 'object') {
@@ -54,11 +9,11 @@ const toNewPatient = (object: unknown): NewPatient => {
 
   if ('name' in object && 'dateOfBirth' in object && 'ssn' in object && 'gender' in object && 'occupation' in object) { 
     const newPatient: NewPatient = {
-      name: parseName(object.name),
-      dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-      ssn: parseSsn(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseOccupation(object.occupation)
+      name: z.string().parse(object.name),
+      dateOfBirth: z.string().date().parse(object.dateOfBirth),
+      ssn: z.string().parse(object.ssn),
+      gender: z.nativeEnum(Gender).parse(object.gender),
+      occupation: z.string().parse(object.occupation)
     };      
     return newPatient;
   }
